f2e08808c8b595b156c53f4ed85771d9e1e1183c
Thomas McMurphy <thmcmurphy@gmail.com>
2016-01-04 13:55:01 -0800

    Update logging in client to use logging library

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 import argparse
-import socket
+import logging
 import messages
+import socket
 
 
 if __name__ == "__main__":
@@ -8,6 +10,8 @@ if __name__ == "__main__":
     parser.add_argument("--address", dest="address", type=str, default="127.0.0.1")
     parser.add_argument("--port", dest="port", type=int, default=9000)
     options = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("client")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((options.address, options.port))
@@ -18,22 +22,20 @@ if __name__ == "__main__":
             if request < 0:
                 raise ValueError
         except ValueError:
-            print "Invalid input: Please enter an unsigned int"
+            logger.error("Invalid input: Please enter an unsigned int")
             request = None
-
-    print "raw request", request
+    logger.debug("request generated: %r" % request)
     request = messages.fib_request.pack(request)
-    print "packed request", repr(request)
-
     sock.sendall(request)
-    response = sock.recv(1024)
+    logger.debug("request sent: %r" % request)
 
-    print "packed response", repr(response)
+    response = sock.recv(1024)
+    logger.debug("response recieved: %r" % response)
     response = messages.fib_response.unpack(response)
-    print "unpacked response", response
+    logger.debug("response unpacked: %r-%r" % (response[0], response[1]))
     if response[0] == 200:
-        print "The answer is: %r" % response[1]
+        logger.info("The answer is: %r" % response[1])
     else:
-        print "Server error: %r-%r" % (response[0], response[1])
-        print "Please check server logs for more details."
+        logger.error("Server error: %r-%r" % (response[0], response[1]))
+        logger.error("Please check server logs for more details.")
     sock.close()
